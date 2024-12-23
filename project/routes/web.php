f360b0ef4efd41206d517d6073f52c5d059cf630
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-12-23 23:06:53 +0800

    added a home controller in the admin and registered the route in the web route

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminHomeController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
 
 Route::get('/', function () {
     return view('welcome');
