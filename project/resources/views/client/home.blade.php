462fb0dc65b3fb88b44602d0c9e538f13fe7e996
Jaspher Cabilin Bagor <141024571+jaspherbagor@users.noreply.github.com>
2025-01-17 21:05:11 +0800

    Update home.blade.php

--- project/resources/views/client/home.blade.php
+++ project/resources/views/client/home.blade.php
@@ -4,11 +4,6 @@
 
 <div class="container-fluid bg-info vh-100 p-3">
     <h2>This is a heading of the homepage</h2>
-    <h3>This is an h3 tag</h3>
-    <h4>H4 tag</h4>
-    ...
-    <h5>....</h5>
-    <h6></h6>
 </div>
 
 @endsection
